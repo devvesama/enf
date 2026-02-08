d08bb7f2f97a5995ec23bb77239b73b5965bf5f7
Bek <103777571+devvesama@users.noreply.github.com>
2026-02-08 23:16:38 +0500

    сделали views/urls/admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Size, Product, \
+    ProductSize, ProductImage
+
+class ProductImageInline(admin.TabularInline):    
+    model = ProductImage
+    extra = 1
+
+
+class ProductSizeInline(admin.TabularInline):    
+    model = ProductSize
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'color', 'price']
+    list_filter = ['category', 'color']
+    search_fields = ['name', 'description' 'color']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline, ProductSizeInline]
+
+class CategoryAdmin(admin.ModelAdmin):    
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+class SizeAdmin(admin.ModelAdmin):    
+    list_display = ['name']
+
+admin.site.register(Category, CategoryAdmin)    
+admin.site.register(Size, SizeAdmin)    
+admin.site.register(Product, ProductAdmin)    
